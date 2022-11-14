7bc1714af5c51c74601bc36c3b577d1dda5a2b2f
john <aarontu803@gmail.com>
2022-11-14 16:05:25 +0800

    data from API

--- src/main.js
+++ src/main.js
@@ -1,58 +1,14 @@
 import "./input.css";
-
-let data = [
-  {
-    id: 0,
-    name: "肥宅心碎賞櫻3日",
-    imgUrl:
-      "https://images.unsplash.com/photo-1522383225653-ed111181a951?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1655&q=80",
-    area: "高雄",
-    description: "賞櫻花最佳去處。肥宅不得不去的超讚景點！",
-    group: 87,
-    price: 1400,
-    rate: 10,
-  },
-  {
-    id: 1,
-    name: "貓空纜車雙程票",
-    imgUrl:
-      "https://images.unsplash.com/photo-1501393152198-34b240415948?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1650&q=80",
-    area: "台北",
-    description:
-      "乘坐以透明強化玻璃為地板的「貓纜之眼」水晶車廂，享受騰雲駕霧遨遊天際之感",
-    group: 99,
-    price: 240,
-    rate: 2,
-  },
-  {
-    id: 2,
-    name: "台中谷關溫泉會1日",
-    imgUrl:
-      "https://images.unsplash.com/photo-1535530992830-e25d07cfa780?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1650&q=80",
-    area: "台中",
-    description:
-      "全館客房均提供谷關無色無味之優質碳酸原湯，並取用八仙山之山冷泉供蒞臨貴賓沐浴及飲水使用。",
-    group: 20,
-    price: 1765,
-    rate: 7,
-  },
-];
+import axios from 'axios';
 
 let addTicket = document.querySelector("#addticket");
 let list = document.querySelector("[data-list]");
 let filterArea = document.querySelector("[data-filter-area]");
 let listNum = document.querySelector("[data-list-num]");
-let notFound = document.querySelector('[data-not-found]')
+let notFound = document.querySelector("[data-not-found]");
+let areaTest = ["台北", "台中", "高雄"];
+let alertMessage = document.querySelectorAll("[data-message]");
 
-filterArea.addEventListener("change", (e) => {
-  list.innerHTML = "";
-  if (e.target.value === "全部地區") {
-    printList(data);
-  } else {
-    let dataList = data.filter(({ area }) => area === e.target.value);
-    printList(dataList);
-  }
-});
 
 
 function printList(data) {
@@ -79,65 +35,92 @@ function printList(data) {
     </li>`;
     }
   );
-  
 
   listNum.textContent = data.length;
   if (data.length === 0) {
-    notFound.classList.remove('hidden');
-  } else if(data.length >=1 ){ 
-    notFound.classList.add('hidden');
-  };
+    notFound.classList.remove("hidden");
+  } else if (data.length >= 1) {
+    notFound.classList.add("hidden");
+  }
 }
 
-
-let areaTest = ["台北", "台中", "高雄"];
-printList(data);
-
-let alertMessage = document.querySelectorAll("[data-message]");
-addTicket.addEventListener("submit", (e) => {
-  e.preventDefault();
-  let dataObj = {
-    id: new Date(),
-    name:
-      e.target["ticket-name"]?.value !== ""
-        ? e.target["ticket-name"]?.value
-        : false,
-    imgUrl:
-      e.target["ticket-imgURL"]?.value !== ""
-        ? e.target["ticket-imgURL"]?.value
-        : false,
-    area: areaTest.some((x) => x == e.target["ticket-area"]?.value)
-      ? e.target["ticket-area"]?.value
-      : false,
-    description:
-      e.target["ticket-description"]?.value !== ""
-        ? e.target["ticket-description"]?.value
-        : false,
-    group:
-      e.target["ticket-group"]?.value !== ""
-        ? e.target["ticket-group"]?.value
-        : false,
-    price:
-      e.target["ticket-price"]?.value !== ""
-        ? e.target["ticket-price"]?.value
-        : false,
-    rate:
-      e.target["ticket-rate"]?.value !== ""
-        ? e.target["ticket-rate"]?.value
-        : false,
-  };
-
-  let values = Object.values(dataObj).slice(1);
-  alertMessage.forEach((item, i) => {
-    console.log(values[i]);
-    values[i] === false
-      ? item.classList.remove("invisible")
-      : item.classList.add("invisible");
-  });
-  if (values.every((x) => x)) {
-    data.push(dataObj);
-    list.innerHTML = "";
-    printList(data);
-    e.target.reset();
+async function getAPI() { 
+  let ticketUrl =
+    "https://raw.githubusercontent.com/hexschool/js-training/main/travelApi.json";
+  try {
+    let res = await axios.get(ticketUrl); 
+    return res
+  } catch(error) { 
+    console.error(error)
   }
-});
+};   
+
+getAPI().then(({ data: { data } }) => { 
+  //初始化
+  printList(data);
+  // //新增套票
+  addTicket.addEventListener("submit", (e) => {
+    e.preventDefault();
+    let dataObj = {
+      id: new Date(),
+      name:
+        e.target["ticket-name"]?.value !== ""
+          ? e.target["ticket-name"]?.value
+          : false,
+      imgUrl:
+        e.target["ticket-imgURL"]?.value !== ""
+          ? e.target["ticket-imgURL"]?.value
+          : false,
+      area: areaTest.some((x) => x == e.target["ticket-area"]?.value)
+        ? e.target["ticket-area"]?.value
+        : false,
+      description:
+        e.target["ticket-description"]?.value !== ""
+          ? e.target["ticket-description"]?.value
+          : false,
+      group:
+        e.target["ticket-group"]?.value !== ""
+          ? e.target["ticket-group"]?.value
+          : false,
+      price:
+        e.target["ticket-price"]?.value !== ""
+          ? e.target["ticket-price"]?.value
+          : false,
+      rate:
+        e.target["ticket-rate"]?.value !== ""
+          ? e.target["ticket-rate"]?.value
+          : false,
+    };
+
+    let values = Object.values(dataObj).slice(1);
+    alertMessage.forEach((item, i) => {
+      values[i] === false
+        ? item.classList.remove("invisible")
+        : item.classList.add("invisible");
+    });
+    if (values.every((x) => x)) {
+      data.push(dataObj);
+      list.innerHTML = "";
+      printList(data);
+      e.target.reset();
+    }
+  });
+  // //篩選地區
+  filterArea.addEventListener("change", (e) => {
+    list.innerHTML = "";
+    if (e.target.value === "全部地區") {
+      printList(data);
+    } else {
+      let dataList = data.filter(({ area }) => area === e.target.value);
+      printList(dataList);
+    }
+  });
+}); 
+
+
+
+
+
+
+
+
